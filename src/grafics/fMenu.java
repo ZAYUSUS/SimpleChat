7ddd65a9dd52037469e22da17a1bd3c4976ef5d6
ZAYUSUS <62956330+ZAYUSUS@users.noreply.github.com>
2020-09-16 18:04:25 -0600

    Se creó el codigo que genera la ventana

--- src/grafics/fMenu.java
+++ src/grafics/fMenu.java
@@ -3,4 +3,19 @@ package grafics;
 import  javax.swing.*;
 
 public class fMenu {
+    public static void main(String[] args ){
+        myWindow marco1= new myWindow(); //crea la ventana
+        marco1.setVisible(true);// vuelve la ventana visible
+        marco1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//hace que la ventana se cierre
+
+    }
+}
+/*
+*Se crea la clase de la ventana que hereda
+* de la calse JFrame
+ */
+class myWindow extends JFrame{
+    public myWindow(){
+        setSize(500,300);
+    }
 }
