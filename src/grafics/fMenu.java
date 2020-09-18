95b10961f0f4254ab0bbd43e5f451df26d984c47
ZAYUSUS <62956330+ZAYUSUS@users.noreply.github.com>
2020-09-18 13:32:11 -0600

    Se le cambio el diseño de la ventana y los elementos de esta

--- src/grafics/fMenu.java
+++ src/grafics/fMenu.java
@@ -6,7 +6,6 @@ import java.awt.*;
 import javax.naming.ldap.UnsolicitedNotification;
 
 import  javax.swing.*;//importa la biblioteca grafica
-import java.awt.*;
 
 public class fMenu {
     public static void main(String[] args ){
@@ -21,6 +20,12 @@ public class fMenu {
 * de la calse JFrame
  */
 class myWindow extends JFrame{
+    //--------------------COLORES---------------------------
+    public Color azulito = new Color(28,232,175);//color de fondo
+    public Color amarillo = new Color(219,190,22);
+    public  Color rosado = new Color(219,22,154);
+    //---------------------------------------------------------
+    public JPanel paper;
     public myWindow(){
         Toolkit screen = Toolkit.getDefaultToolkit();
         Dimension screenSize=screen.getScreenSize();
@@ -31,36 +36,41 @@ class myWindow extends JFrame{
         setSize(screenHeight/2,screenWidth/4);
         setTitle("Mensager");
         setLocation(screenHeight/4,screenWidth/6);
+        setBackground(Color.PINK);
         setResizable(false);
-
-        Paper plank = new Paper();
-        add(plank);
 
         setVisible(true);// vuelve la ventana visible
 
+        Inicializador();
+
+    }
+    private void Inicializador(){
+        PaperCreator();
+        ComponentesEtiquetas();
+        Botones();
+    }
+    private void PaperCreator(){//Clase que dibuja en la ventana funciona para introducir imagenes,texto, botones...
+        paper = new JPanel();
+        paper.setLayout(null);//desactiva el diseño de el layout
+        paper.setBackground(azulito);
+        this.getContentPane().add(paper);
+    }
+    private void ComponentesEtiquetas(){
+
+        //-----------------------------------------------------------------------
+        JLabel etiqueta = new JLabel("Mensajería",SwingConstants.LEFT);//etiqueta del titulo
+        etiqueta.setBounds(0,0,400,50);
+        etiqueta.setOpaque(true);//activa la opcion de editar el color de la etiqueta
+        etiqueta.setBackground(amarillo);
+        etiqueta.setFont(new Font("Century Gothic",Font.PLAIN,45));
+        paper.add(etiqueta);//etiqueta se agrega al panel
+    }
+    private void Botones(){
+        JButton enviar = new JButton("Enviar");
+        enviar.setBounds(20,400,100,30);
+        enviar.setBackground(rosado);//Agrega color al boton
+        enviar.setFont(new Font("Daytona Pro Light",Font.PLAIN,20));
+        paper.add(enviar);//Añade el boton a la ventana
     }
 }
 
-class Paper extends JPanel{
-
-    public Paper (){
-        setLayout(new BorderLayout());
-
-        JPanel cuadroBlanco = new JPanel();
-
-        JMenuBar menu = new JMenuBar();
-
-        JMenu enviar = new JMenu("Enviar");
-
-        menu.add(enviar);
-
-        cuadroBlanco.add(menu);
-
-        add(cuadroBlanco,BorderLayout.SOUTH);
-        //-------------------------------------------
-
-        JTextPane body = new JTextPane();
-        add(body,BorderLayout.CENTER);
-
-    }
-}
